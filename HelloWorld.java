f71854d5f57ed1a12f8b62f4e04d19e2b44d4ac2
espy333 <99069517+espy333@users.noreply.github.com>
2024-08-30 12:50:40 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+  static void method(){
+    System.out.println("Hello java method");
+    System.out.println("Hello changes java method");
+  }
+  public static void main(String[] args){
+    System.out.println("Hello changes java method");
+    method();    
+}
+}
