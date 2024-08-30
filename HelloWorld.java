fa764f331a6fff72765957946d879f3f06bce8f9
espy333 <you@example.com>
2024-08-30 14:16:25 +0530

    changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
   static void method(){
     System.out.println("Hello java method");
     System.out.println("Hello changes java method");
+System.out.println("Hello hello");
   }
   public static void main(String[] args){
     System.out.println("Hello changes java method");
